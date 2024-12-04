173a3851e69dc9e6489839a156418a50db7f0a79
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-04 15:23:16 +0200

    created the go back button in the startScreen

--- react-quiz/src/components/StartScreen.js
+++ react-quiz/src/components/StartScreen.js
@@ -1,4 +1,8 @@
+import {useQuiz} from "../context/QuizContext";
+
 function StartScreen({numQuestions, dispatch}) {
+    const {quiz, setQuiz} = useQuiz();
+
     return (
         <div className="start">
             <h3>Welcome to the react quiz!!</h3>
@@ -6,8 +10,8 @@ function StartScreen({numQuestions, dispatch}) {
             <button onClick={() => dispatch({type: "start"})} className="btn btn-ui">
                 Let's start
             </button>
-            <button onClick={() => dispatch({type: "start"})} className="btn btn-ui">
-                Let's start
+            <button onClick={() => setQuiz("choosing")} className="btn btn-ui">
+                Go Back
             </button>
         </div>
     );
