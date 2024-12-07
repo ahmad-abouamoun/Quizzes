2f7961df54bda1775642171fbd91b1a9622430f2
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-07 19:27:30 +0200

    created the connection with the db

--- server/index.js
+++ server/index.js
@@ -1,3 +1,12 @@
 import express from "express";
-
+import connectToDatabase from "./connect.js";
 const app = express();
+
+app.get("/hello", (req, res) => {
+    res.send("hello");
+});
+
+app.listen(8080, () => {
+    console.log("hello world");
+    connectToDatabase();
+});
