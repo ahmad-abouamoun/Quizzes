3e9fac8273e86c048a32748c1753ed576fb755aa
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-09 08:51:44 +0200

    moved the getUsers api from the routes to the contriller

--- server/Routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-import {User} from "../Models/user";
-
-const getUsers = async (req, res) => {
-    const id = res.params.id;
-    if (!id) {
-        const users = User.find();
-        return res.json(users);
-    }
-};
